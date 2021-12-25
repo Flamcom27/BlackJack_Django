12d12d5d47472f2787a78bf88981162349126185
Flamcom27 <27flamcom06@gmail.com>
2021-12-25 16:08:35 +0300

    class card is created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Card:
+    def __init__(self, rank, suit):
+        self.suit = suit
+        if rank == str:
+            if rank == 'A':
+                self.name = f'{rank}_of_{suit}'
+                self.rank = (1, 11)
+            else:
+                self.name = f'{rank}_of_{suit}'
+                self.rank = 10
+        else:
+            self.name = f'{rank}_of_{suit}'
+            self.rank = rank
+
+    def __repr__(self):
+        return self.name
+
+
+# class Deck(models.Model):
+#     def generate_deck(self):
+#         pass
+
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import models
 
-# Create your tests here.
+
+# Create your tests here
+
+def test_card():
+    ranks = ('A', 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K')
+    suits = ('diamonds', 'clubs', 'hearts', 'spades')
+    list_of_cards = [models.Card(i, j) for i in ranks for j in suits]
+    print(list_of_cards)
+
